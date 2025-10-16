2cc2ecea614e532c27baa72c04a1b16019b8be0e
Otatopot <rochelewebs24@gmail.com>
2025-10-16 10:52:25 -0500

    Added X and O onclick function and state array

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,6 +2,76 @@ function grid(){
     const gridsquares = document.getElementById("board").children;
     for (var i = 0; i < gridsquares.length; i++) {
         gridsquares[i].setAttribute("class", "square")
+        gridsquares[i].setAttribute("id", "square_"+i)
 } 
+
+
+    const squares = document.getElementsByClassName("square");
+    let myArray = [];
+    let playCount = [];
+    let non_null = myArray.filter(Boolean); 
+    let count_nn = 0;
+    for (let i = 0; i < squares.length; i++){
+        //squares[i].addEventListener("hover", )
+        squares[i].addEventListener("click", e => {
+            if (myArray[i] == null){
+                const state = e.target.getAttribute("class");
+                if (state == "square"){
+                    e.target.setAttribute("class", "square X");
+                    e.target.innerHTML = "X";
+                }
+                if (state == "square X"){
+                    e.target.setAttribute("class", "square O");
+                    e.target.innerHTML = "O";
+                }
+                if (state == "square O"){
+                    e.target.setAttribute("class", "square X");
+                    e.target.innerHTML = "X";
+                }
+                //If this is the first click, save value to array.
+                myArray[i] = e.target.innerHTML;
+                //save the number of values in the array to the matching index in 
+                //playCount
+                non_null = myArray.filter(Boolean);
+                count_nn = non_null.length;
+
+                playCount[i] = count_nn
+            }
+            else {
+                //If the another square has been pressed (and another array value added to the list)
+                //do not allow them to click again. But if the values match, no other square was changed and a 
+                //click is allowed
+                if (playCount[i] == count_nn){
+                    const state = e.target.getAttribute("class");
+                    if (state == "square"){
+                        e.target.setAttribute("class", "square X");
+                        e.target.innerHTML = "X";
+                    }
+                    if (state == "square X"){
+                        e.target.setAttribute("class", "square O");
+                        e.target.innerHTML = "O";
+                    }
+                    if (state == "square O"){
+                        e.target.setAttribute("class", "square X");
+                        e.target.innerHTML = "X";
+                    }
+                    //If this is the first click, save value to array.
+                    myArray[i] = e.target.innerHTML;
+                }
+                console.log(count_nn, myArray)
+            }
+            
+            
+            
+
+        });
+
+    }
+    //function() {innerHTML = "x";}
+    //document.getElementById("demo").innerHTML = sometext;
 }
+
 window.addEventListener("DOMContentLoaded", grid);
+
+
+
